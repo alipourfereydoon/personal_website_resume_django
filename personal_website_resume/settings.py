e75e126956a9959487557bfb06ad7b3d87ad4d9c
ali.pourfereydoon <pourfereydoonali@gmail.com>
2025-07-24 02:12:48 -0700

    create account app , then add login form

--- personal_website_resume/settings.py
+++ personal_website_resume/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home_app.apps.HomeAppConfig',
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
@@ -115,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR ,'statics')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
