dd42b5d9208bae4c851d59bdf20c68a67afe56fb
ali.pourfereydoon <pourfereydoonali@gmail.com>
2025-07-28 23:40:23 -0700

    pagination:in article_list.html we have many articles and it is not good to show all articles in one page , therefore we use pagination system to show atleast 1 or two article in one page

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render,get_object_or_404
 from . models import Article,Category
+from django.core.paginator import Paginator
 
 
 def article_detail(request,pk):
-    article = get_object_or_404(Article,id = pk)
-    return render(request,'blog_app/article_detail.html',context={'article':article})
+    articles = get_object_or_404(Article,id = pk)
+    return render(request,'blog_app/article_detail.html',context={'article':articles})
 
 
 
 def allpost(request):
     articles = Article.objects.all()
+    paginator = Paginator(articles,1)
+    page_number = request.GET.get('page')
+    object_list = paginator.get_page(page_number)
     recents = Article.objects.order_by('-created')[:3]
     return render(request,'blog_app/article_list.html',
-                  {'articles':articles,
+                  {'articles':object_list,
                    'recent_post':recents
                    })
 
